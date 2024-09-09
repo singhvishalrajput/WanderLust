e8dc66b8526644f6f734133cca6a41cf512b8b2d
Vishal Singh <singh287686@gmail.com>
2024-09-09 14:21:31 +0530

    Added signup form layout in signup.ejs with input fields for username, email, and password, styled with Bootstrap for Wanderlust project.

--- app.js
+++ app.js
@@ -12,9 +12,16 @@ const {listingSchema, reviewSchema} = require("./schema.js")
 const Review  = require("./models/review.js")
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js")
 
-const listings = require("./routes/listing.js");
-const reviews  = require("./routes/review.js")
+
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter  = require("./routes/review.js")
+const userRouter  = require("./routes/user.js")
+
 
 
 app.set("view engine", "ejs");
@@ -54,15 +61,33 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req, res, next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
 
+app.get("/demouser", async (req, res)=>{
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "vishal"
+    });
+    let registeredUser = await User.register(fakeUser, "helloworld"); //User="fakeUser" , password="helloworld"
+    res.send(registeredUser);
+})
 
-app.use("/listing", listings);
-app.use("/listing/:id/reviews", reviews)
+
+app.use("/listing", listingRouter);
+app.use("/listing/:id/reviews", reviewRouter)
+app.use("/", userRouter);
 
 
 
