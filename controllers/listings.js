436697bd9110dcfdb769c6d892445c2083010fe1
Vishal Singh <singh287686@gmail.com>
2024-09-18 09:17:32 +0530

    Implemented Cloudinary setup and modified routes and views for image uploads

--- controllers/listings.js
+++ controllers/listings.js
@@ -27,8 +27,13 @@ module.exports.showListing = async (req, res)=>{
 }
 
 module.exports.createListing = async (req, res, next)=>{
+    let url = req.file.path;
+    let filename = req.file.filename;
+    
     let newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url, filename};
+
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listing");
@@ -46,9 +51,16 @@ module.exports.renderEditForm = async(req, res)=>{
 }
 
 module.exports.updateListing = async(req, res)=>{
-
     let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
+
+    if(typeof req.file !== "undefined"){
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = {url, filename};
+        await listing.save();
+    }
+
     req.flash("success", "Listing Updated!");
     res.redirect(`/listing/${id}`);
 }
--- routes/listing.js
+++ routes/listing.js
@@ -7,17 +7,17 @@ const {isLoggedIn, isOwner, validateListing} = require("../middleware.js")
 
 const listingController = require("../controllers/listings.js");
 const multer = require('multer');
-const upload = multer({ dest : 'uploads/'}) // It will create a folder automatically.
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage }) // It will create a folder automatically.
 
 
 //Index route and Create route
 router
     .route("/")
     .get( wrapAsync(listingController.index))
-    // .post( validateListing, wrapAsync(listingController.createListing))
-    .post( upload.single('listing[image.url]'), (req, res)=>{
-        res.send(req.file);
-    })
+    .post( isLoggedIn, upload.single('listing[image.url]'), wrapAsync(listingController.createListing));
+
+    
 
 
 
@@ -28,7 +28,7 @@ router.get("/new", isLoggedIn, listingController.renderNewForm)
 router
     .route("/:id")
     .get( wrapAsync(listingController.showListing))
-    .put( isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+    .put( isLoggedIn, isOwner, upload.single('listing[image]'), wrapAsync(listingController.updateListing))
     .delete( isLoggedIn, isOwner,  wrapAsync(listingController.destroyListing));
 
 
